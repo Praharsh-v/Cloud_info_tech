8de771f4efeabaa289c0153fc2ea098ab6eb2b63
Praharsh <praharsh@Praharshs-MacBook-Pro.local>
2022-03-14 00:33:33 +0530

    7th changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,6 +123,10 @@ $(function () {
   if ($(window).scrollTop() > 100) {
     $('#header').addClass('header-scrolled');
   }
+  $(function(){
+  $("#head").load("head.html"); 
+  });
+
 
   // Navigation active state on scroll
   var nav_sections = $('section');
@@ -241,5 +245,7 @@ $(function () {
   $(window).on('load', function() {
     aos_init();
   });
+
+  document.addEventListener("DOMContentLoaded",function(){let e=document.getElementsByTagName("include");for(var t=0;t<e.length;t++){let a=e[t];n(e[t].attributes.src.value,function(e){a.insertAdjacentHTML("afterend",e),a.remove()})}function n(e,t){fetch(e).then(e=>e.text()).then(e=>t(e))}});
 
 })(jQuery);
